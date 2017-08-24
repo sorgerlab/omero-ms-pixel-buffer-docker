81ae94826ee662003cbd26f875ea97f5fcd70c6d
dpwrussell <douglas_russell@hms.harvard.edu>
2017-08-24 13:15:14 -0400

    OMERO pixel buffer micro service docker build

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -eu
+
+OMEROHOST=${OMEROHOST:-omero}
+REDISHOST=${REDISHOST:-redis}
+OMERO_DATA = ${OMERO_DATA:-/OMERO}
+DBHOST=${DBHOST:-db}
+DBUSER=${DBUSER:-postgres}
+DBPASSWORD=${DBPASSWORD:-postgres}
+
+sed -i -re "s/(\"uri\":\s*\"redis:\/\/).*(@).*(:6379\/0\")/\1$REDIS_PASSWORD\2$REDISHOST\3/" /opt/omero/ms/omero-ms-pixel-buffer/conf.json
+sed -i -re "s/(\"host\":\s*\").*(\")/\1$OMEROHOST\2/" /opt/omero/ms/omero-ms-pixel-buffer/conf.json
+sed -i -re "s#(\"omero\.data\.dir\":\s*\").*(\")#\1$OMERO_DATA\2#" /opt/omero/ms/omero-ms-pixel-buffer/conf.json
+sed -i -re "s/(\"omero\.db\.host\":\s*\").*(\")/\1$DBHOST\2/" /opt/omero/ms/omero-ms-pixel-buffer/conf.json
+sed -i -re "s/(\"omero\.db\.user\":\s*\").*(\")/\1$DBUSER\2/" /opt/omero/ms/omero-ms-pixel-buffer/conf.json
+sed -i -re "s/(\"omero\.db\.pass\":\s*\").*(\")/\1$DBPASSWORD\2/" /opt/omero/ms/omero-ms-pixel-buffer/conf.json
+
+/opt/omero/ms/omero-ms-pixel-buffer/bin/omero-ms-pixel-buffer -conf /opt/omero/ms/omero-ms-pixel-buffer/conf.json
