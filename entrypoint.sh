3918b6532ebe4609d68d7e660d3ffd26c3439d86
dpwrussell <douglas_russell@hms.harvard.edu>
2017-08-25 14:59:32 -0400

    Try setting database name

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ OMEROHOST=${OMEROHOST:-omero}
 REDISHOST=${REDISHOST:-redis}
 OMERO_DATA=${OMERO_DATA:-/OMERO}
 DBHOST=${DBHOST:-db}
+DBNAME=${DBNAME:-postgres}
 DBUSER=${DBUSER:-postgres}
 DBPASSWORD=${DBPASSWORD:-postgres}
 
@@ -13,6 +14,7 @@ sed -i -re "s/(\"uri\":\s*\"redis:\/\/).*(@).*(:6379\/0\")/\1$REDIS_PASSWORD\2$R
 sed -i -re "s/(\"host\":\s*\").*(\")/\1$OMEROHOST\2/" /opt/omero/ms/omero-ms-pixel-buffer/conf.json
 sed -i -re "s#(\"omero\.data\.dir\":\s*\").*(\")#\1$OMERO_DATA\2#" /opt/omero/ms/omero-ms-pixel-buffer/conf.json
 sed -i -re "s/(\"omero\.db\.host\":\s*\").*(\")/\1$DBHOST\2/" /opt/omero/ms/omero-ms-pixel-buffer/conf.json
+sed -i -re "s/(\"omero\.db\.name\":\s*\").*(\")/\1$DBNAME\2/" /opt/omero/ms/omero-ms-pixel-buffer/conf.json
 sed -i -re "s/(\"omero\.db\.user\":\s*\").*(\")/\1$DBUSER\2/" /opt/omero/ms/omero-ms-pixel-buffer/conf.json
 sed -i -re "s/(\"omero\.db\.pass\":\s*\").*(\")/\1$DBPASSWORD\2/" /opt/omero/ms/omero-ms-pixel-buffer/conf.json
 
